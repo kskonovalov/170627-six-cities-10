1df6e7f15e4215158cd39e60fd11f1345bc66ab4
Konstantin <picprofit@gmail.com>
2022-07-12 20:42:08 +0200

    fix: issues, add: 404 template

--- project/src/components/ux/card/card.tsx
+++ project/src/components/ux/card/card.tsx
@@ -16,9 +16,7 @@ const Card = () => (
           <b className="place-card__price-value">&euro;120</b>
           <span className="place-card__price-text">&#47;&nbsp;night</span>
         </div>
-        <button className="place-card__bookmark-button button" type="button">
-          {/*<button  className="place-card__bookmark-button place-card__bookmark-button--active button"*/}
-          {/*                type="button">*/}
+        <button className="place-card__bookmark-button button place-card__bookmark-button--active" type="button">
           <svg className="place-card__bookmark-icon" width="18" height="19">
             <use xlinkHref="#icon-bookmark"></use>
           </svg>
